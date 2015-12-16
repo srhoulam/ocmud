2b714b9c5538faf387dc3df6544d55cb39eca3fb
Saad Rhoulam <saad@rhoulam.com>
2015-12-16 18:01:46 -0500

    Upgrade index file to grab all sibling files and register them as models.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var mongoose = require('mongoose');
 
-mongoose.model('Location', require('./Location'));
+fs.readdirSync(__dirname).forEach(function(file) {
+    if(file === 'index.js') {
+        return;
+    }
 
+    var modelName = file.split('.js')[0];
+    var modulePath = path.join(__dirname, file);
+
+    mongoose.model(modelName, require(modulePath));
+});
+
+module.exports = mongoose;
