170d26ce450c8f0dd32d2f883a20f42dafa816ae
Saad Rhoulam <saad@rhoulam.com>
2015-12-28 20:20:31 -0500

    Make user model auth-capable

--- models/User.js
+++ models/User.js
@@ -1,22 +1,69 @@
 'use strict';
 
+var bcrypt = require('bcrypt');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
 
 var userSchema = new Schema({
-    ipAddr : {
+    name : {
         type : String,
-        required : true
+        required : true,
+        unique : true
     },
-    userAgent : {
-        type : String,
-        required : true
-    },
+    digest : String,
+    // phone : String, // for twilio integration
     locations : {
         type : [Schema.Types.ObjectId],
         ref : 'Location',
         default : []
     }
 });
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.methods.comparePassword = function(password) {
+    var self = this;
+
+    return new Promise(function(res, rej) {
+        bcrypt.compare(password, self.digest, function(err, match) {
+            if(err) {
+                rej(err);
+                return;
+            }
+
+            res(match);
+        });
+    });
+};
+
+userSchema.methods.setPassword = function(password) {
+    var self = this;
+
+    return (new Promise(function saltExec(res, rej) {
+        bcrypt.genSalt(16, function(err, salt) {
+            if(err) {
+                rej(err);
+                return;
+            }
+
+            res(salt);
+        });
+    })).then(function(salt) {
+        return new Promise(function hashExec(res, rej) {
+            bcrypt.hash(password, salt, function(err, digest) {
+                if(err) {
+                    rej(err);
+                    return;
+                }
+
+                res(digest);
+            });
+        });
+    }).then(function(digest) {
+        self.digest = digest;
+        return self.save();
+    });
+};
 
 module.exports = userSchema;
