aa4550b998d41a49452a63d2df9658fc5b46a722
Saad Rhoulam <saad@rhoulam.com>
2016-01-04 22:25:11 -0500

    Add purpose comment.

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/** DOC
+ *
+ *  The purpose of the socket.io potion of this application
+ *    is for the (read-only) "explore" mode on the front-end.
+ *
+ */
+
 function onConnection(socket) {}
 
 module.exports = onConnection;
