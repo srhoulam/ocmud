4d31f32b477e15eca33be5e815704927e8be565b
Saad Rhoulam <saad@rhoulam.com>
2016-01-08 22:50:51 -0500

    Factor socket controller module to expose a `setup` function for use in bin/www.
    Also write some socket controller logic to `onConnection`.

--- controller/socket.js
+++ controller/socket.js
@@ -7,6 +7,57 @@
  *
  */
 
-function onConnection(socket) {}
+var Location = require('../models').model('Location');
 
-module.exports = onConnection;
+var totalConnections = 0;
+var io = null;
+
+const direction = ['n', 'e', 'w', 's'];
+const directionNames = ['north', 'east', 'west', 'south'];
+
+function onConnection(socket) {
+    Location.findRandom().limit(1).exec().then(function(loc) {
+        socket.location = loc;
+        socket.join('numClients');
+    }).then(function() {
+        socket.on('disconnect', function() {
+            io.emit('numClients', {
+                clients : --totalConnections
+            });
+        });
+
+        socket.on('command', function(cmd) {
+            var dirIndex = direction.indexOf(cmd);
+            if(dirIndex >= 0) {
+                Location.findById(socket.location[cmd]).exec().then(function(loc) {
+                    socket.location = loc;
+                    socket.emit('info', "You move " + directionNames[dirIndex] + ".");
+                });
+            } else if(cmd === 'look') {
+                Object.keys(socket.location.toObject()).filter(function(elem) {
+                    return socket.location[elem] !== socket.location.id &&
+                        socket.location[elem] !== '__v' &&
+                        socket.location[elem] !== 'random';
+                }); // list of location features, e.g., exits
+            }
+        });
+    }).then(function() {
+        io.emit('numClients', {
+            clients : ++totalConnections
+        });
+    }).catch(function(err) {
+        socket.disconnect();
+    });
+}
+
+function setup(argIo) {
+    io = argIo;
+
+    // set CORS origins
+    io.origins('*'); // for now
+
+    // set connection handler
+    io.on('connection', onConnection);
+}
+
+module.exports = setup;
