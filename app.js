12985dabd09966a9b3129555a31b6d3a7f713f92
Saad Rhoulam <saad@rhoulam.com>
2016-01-20 22:02:11 -0500

    'saveUnitalized' -> 'saveUninitialized'

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
   store : new MongoStore({
     mongooseConnection : db.connection
   }),
-  saveUnitialized : false,
+  saveUninitialized : false,
   resave : false,
   genid : function() {
     return uuid.v4({
